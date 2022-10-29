4337e4e00ef31d520393294fc4f7c69479199a26
giulliana234 <116364106+giulliana234@users.noreply.github.com>
2022-10-28 19:02:57 -0500

    Add files via upload

---
+++ multplicacion.php
@@ -0,0 +1,10 @@
+<?php 
+
+$multiplicando= 5;
+$multiplicador;
+
+for ($multiplicador=1; $multiplicador <=10 ; $multiplicador++) { 
+	echo "$multiplicando" . " X " . $multiplicador . " = " . $multiplicando * $multiplicador;
+	echo "<br>";
+}
+?>
